26376bfaecd3a4de97bd1ef68c9e7976349136c5
gavrilov-si25 <mailpochtaforme@gmail.com>
2024-11-17 20:12:23 +0000

    Task 4. Project styling

--- scripts/info.js
+++ scripts/info.js
@@ -1,4 +1,7 @@
 const features = document.querySelectorAll('.feature');
+if (features.length > 0) {
+    features[0].classList.add('active');
+}
 features.forEach(feature => {
     feature.addEventListener('click', () => {
         features.forEach(f => f.classList.remove('active'));
